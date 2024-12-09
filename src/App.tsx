e9cf8b7ed01558f17a1bd7a7a66456fce7545d4e
Shanmukhanath Ravuri <sravuri088@everestdx.com>
2024-12-10 03:44:29 +0530

    added lint, husky packages

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 const Home: React.FC = () => {
-  return <h1>Home Page</h1>;
+  return <h3>Home Page</h3>;
 };
 
 const About: React.FC = () => {
-  return <h1>About Page</h1>;
+  return <h3>About Page</h3>;
+};
+
+const PageNotFound: React.FC = () => {
+  return <h3>Error:( Page not Found</h3>;
 };
 
 const App: React.FC = () => {
@@ -25,6 +29,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </Router>
   );
