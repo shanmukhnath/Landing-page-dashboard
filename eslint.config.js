3c7e8ad89a2385186fcc4707f2f89c8e165a1664
Shanmukhanath Ravuri <sravuri088@everestdx.com>
2024-12-10 14:20:39 +0530

    removed prettier

--- eslint.config.js
+++ eslint.config.js
@@ -3,19 +3,30 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-
-/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: { 
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      }
+    }
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-  'react/react-in-jsx-scope': 'off',
-];
-
-module.exports = {
-  rules: {
-    'react/react-in-jsx-scope': 'off',
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+    }
   }
-};
+];
