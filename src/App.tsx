87c2f4dc9dd40e0824cbc4ffaf63204773298ada
Shanmukhanath Ravuri <sravuri088@everestdx.com>
2024-12-10 14:25:30 +0530

    changed text

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 const Home: React.FC = () => {
-  return <h3>Home Page</h3>;
+  return <h3>Home Pages</h3>;
 };
 
 const About: React.FC = () => {
-  return <h3>About Page</h3>;
+  return <h3>About Pages</h3>;
 };
 
 const PageNotFound: React.FC = () => {
