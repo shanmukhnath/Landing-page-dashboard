647ab52c6a3e9309815e40e53a654a85535b771a
Shanmukhanath Ravuri <sravuri088@everestdx.com>
2024-12-23 19:44:31 +0530

    changed eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -3,30 +3,25 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
+
+/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-    languageOptions: { 
-      globals: {
-        ...globals.browser,
-        ...globals.node
-      },
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true
-        },
-        ecmaVersion: 'latest',
-        sourceType: 'module'
-      }
-    }
-  },
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {languageOptions: { globals: globals.browser }},
+ { settings: {
+    react: {
+      version: "detect",
+    },}
+},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
     rules: {
+      "eqeqeq": ["error", "always"],
+      "no-unused-vars": "error",
+      "prefer-const": ["error", { "ignoreReadBeforeAssign": true }],
       'react/react-in-jsx-scope': 'off',
-      'react/jsx-uses-react': 'off',
-    }
   }
+}
 ];
